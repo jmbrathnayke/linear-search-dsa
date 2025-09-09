b9c5cedcd83a2e6b9ea4027000475cb414cf6e24
Manujayaa <janithrathnayake01@gmail.com>
2025-09-09 12:45:27 +0530

    linear search & binary search

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,8 @@ public class Main {
         int target = 11;
 
         // Call linearSearch method and store the result (index of target or -1)
-        int result = linearSearch(nums, target);
+       // int result = linearSearch(nums, target);
+        int result = binarySearch(nums,target);
 
         // Check if the element was found and print appropriate message
         if (result != -1)
@@ -25,6 +26,31 @@ public class Main {
             // If the current element matches the target, return its index
             if (nums[i] == target)
                 return i;
+        }
+
+        // If target is not found, return -1
+        return -1;
+    }
+
+    private static int binarySearch(int[] nums, int target) {
+
+        //3, 5, 7, 11, 13
+        //starting and ending point
+        int left = 0;
+        int right = nums.length-1;
+
+        //check condition
+        while(left <= right){
+            int mid = (left + right)/2;
+
+            if(nums[mid] == target){
+               return mid; //index
+            } else if (nums[mid] < target) {
+                //shift left
+                left = mid + 1;
+            }else{
+                right = mid -1;
+            }
         }
 
         // If target is not found, return -1
